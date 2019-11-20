94900f427763ae421f062243a9276dd1f14f6012
yanan-gj <gaojian19961105@gmail.com>
2019-11-20 19:32:29 +0800

    gj

--- src/main/java/com/qf/controller/PropertyController.java
+++ src/main/java/com/qf/controller/PropertyController.java
@@ -1,11 +1,14 @@
 package com.qf.controller;
 
 import com.alipay.api.AlipayApiException;
+import com.qf.domain.User;
+import com.qf.service.AcountService;
 import com.qf.utils.AlipayUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.boot.web.servlet.server.Session;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * @Company: Telegram
@@ -17,17 +20,20 @@ import org.springframework.web.bind.annotation.SessionAttribute;
 public class PropertyController {
     @Autowired
     private AlipayUtils alipayUtils;
-
-    @RequestMapping("/propertypay")
-    public String PropertyPay(){
-
+    @Autowired
+   private AcountService acountService;
+    @RequestMapping( "/propertypay")
+   public String PropertyPay(HttpSession httpSession){
+        User user =(User) httpSession.getAttribute("user");
+        Integer userId = user.getUserId();
+        acountService.save(userId);
         String pay="";
         try {
             pay = alipayUtils.pay();
         } catch (AlipayApiException e) {
             e.printStackTrace();
         }
-        System.out.println(pay);
+
         return pay;
     }
 }
---
+++ src/main/java/com/qf/service/AcountService.java
@@ -0,0 +1,11 @@
+package com.qf.service;
+
+/**
+ * @Company: Telegram
+ * @Author: 高健
+ * @Date: 2019/11/20
+ * @Time: 17:33
+ */
+public interface AcountService {
+    Integer save (Integer userId);
+}
