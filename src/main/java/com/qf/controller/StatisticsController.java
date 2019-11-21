1df3e4a1f6c70e07b1f7a552da1442df8e730800
luanqi77 <623233604@qq.com>
2019-11-21 19:45:13 +0800

    Merge branch 'master' of https://github.com/luanqi77/property-item
    # Conflicts:
    #	.idea/workspace.xml

--- src/main/java/com/qf/controller/StatisticsController.java
+++ src/main/java/com/qf/controller/StatisticsController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.text.DecimalFormat;
 import java.text.Format;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class StatisticsController {
@@ -16,15 +18,18 @@ public class StatisticsController {
     /*
     * 物业缴费率*/
     @RequestMapping("/getContributionRate")
-    public String getContributionRate(@RequestParam("build") Integer build){
-
-        Integer noArrears = houseService.selectNoArrears(build);
-        Integer household = houseService.selectHousehold(build);
-        Format decimalFormat = new DecimalFormat("0.00");
-        String format = decimalFormat.format((double) noArrears / household);
-        System.out.println(format);
-
-        return format;
+    public  List<String> getContributionRate(){
+        List<String> list = new ArrayList<>();
+        String format="";
+        for (int i = 1; i < 6; i++) {
+            Integer noArrears = houseService.selectNoArrears(i);
+            Integer household = houseService.selectHousehold(i);
+            Format decimalFormat = new DecimalFormat("0.00");
+            format = decimalFormat.format((double) noArrears / household);
+            list.add(format);
+            System.out.println(format);
+        }
+        return list;
     }
 
 }
